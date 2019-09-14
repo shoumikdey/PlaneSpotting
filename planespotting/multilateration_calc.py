c1329d4f8d6c4ab106dae94b60104a913edfcbe5
Shoumik Dey <shoumikdey123@gmail.com>
2019-09-15 02:09:26 +0530

    Building list of known ground stations

--- planespotting/multilateration_calc.py
+++ planespotting/multilateration_calc.py
@@ -2,4 +2,6 @@ from planespotting import utils
 
 def get_all_station_data():
     data = utils.load_json("planespotting/gs_data.json")
-    print(data)
+    known_st = []
+    for i in data:
+        known_st.append([data[i]['lat'], data[i]['lon'], data[i]['alt']])
